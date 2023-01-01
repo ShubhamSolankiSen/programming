3be6b4475eed5758226798dedf9f7cd342ecbbaa
Shubham Solanki <121003151+ShubhamSolankiSen@users.noreply.github.com>
2023-01-01 13:27:22 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a,b,c;
+    cout<<"Enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"Enter the value of b:"<<endl;
+    cin>>b;
+c = a + b;
+cout<<"the sum is"<<c;
+
+return 0;
+}
+
+    
+    
+    
+    
---
+++ tut3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 54; i++)
+    {
+        cout<<i<<endl;
+        if(i==4){
+            break;
+        }
+    }
+    
+
+    return 0;
+}
