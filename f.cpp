4df3a5be682876f97a9a3fd3f28a972e9d080f24
Shubham Solanki <121003151+ShubhamSolankiSen@users.noreply.github.com>
2023-01-01 13:35:20 +0530

    Add files via upload

---
+++ f.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b) {
+int c = a + b;
+return c;
+}
+
+int main(){
+    
+    cout<<"The sum of 11 and 5 is "<<sum(11,5);
+    return 0;
+}
