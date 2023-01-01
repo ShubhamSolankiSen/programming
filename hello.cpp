fbabe904facb9a590355cf69dea83854204d8ca5
Shubham Solanki <121003151+ShubhamSolankiSen@users.noreply.github.com>
2023-01-01 13:37:16 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void hello();
+void hii();
+void bye();
+int main()
+{
+    hello();
+    hii();
+    bye();
+    return 0;
+}
+void hello()
+{
+    cout<<"hello"<<endl;
+}
+void hii()
+{
+    cout<<"hii"<<endl;
+}void bye()
+{
+    cout<<"bye"<<endl;
+}
+
