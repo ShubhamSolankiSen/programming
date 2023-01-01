30a346e74fba77028c7cdf6b8c40f00f86b1aed6
Shubham Solanki <121003151+ShubhamSolankiSen@users.noreply.github.com>
2023-01-01 13:33:17 +0530

    Add files via upload

---
+++ cb.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class person
+{
+char name[30];
+int age;
+public:
+void getdata();
+void display();
+
+};
+void person::getdata()
+{
+    cout<<"Enter your name:"<<endl;
+    cin>>name;
+    cout<<"Enter your age:";
+    cin>>age;
+}
+void person ::display()
+{
+cout<<" name :"<<name<<endl;
+cout<<" age :"<<age<<endl;
+}
+#include<iostream>
+using namespace std;
+
+int main(){
+    person p;
+    p.getdata();
+    p.display();                    
+    
+    return 0;
+}
